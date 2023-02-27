22d6a35adcfc949b8dfadcd2edc6c0692a51877d
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2023-02-27 20:11:48 +0530

    css

--- src/component/WeatherApp/Wheather_App.jsx
+++ src/component/WeatherApp/Wheather_App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 const Wheather = () => {
     const [weather_data, setWeatherData] = useState([])
-    const [History, setHistory] = useState("")
+    // const [History, setHistory] = useState("")
     const [search, setSearch] = useState("")
     useEffect(() => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=55c09104f85830eee3e8b8ee0f804dca`).then((res) => {
@@ -16,27 +16,30 @@ const Wheather = () => {
 
     return (
         <>
-        <h2>Weather App</h2>
-            <input type="text" placeholder="Enter a City name" onChange={(e) => { setSearch(e.target.value) }} style={{ margin: "40px auto", width: "300px", height: "35px", padding: "5px" }} />
-            {!search ?
-                <p>Enter Valid City</p>
-                :
-                <div>
-                    {weather_data.map((items, i) => {
-                        return (
-                            <div style={{ width: "300px", height: "auto", border: "1px solid black", backgroundColor: "grey", margin: "auto" }}>
-                                <p>Weather Details of City : {items.name}</p>
-                                <p>Current Temparature: {items.cod - 173} &deg;C</p>
-                                <p>Temparature Range: {items.cod - 180} &deg;C to {items.cod - 170} &deg;C</p>
-                                <p>Humidity: {items.cod - 156}</p>
-                                <p>Sea-level:{items.visibility - 1008}</p>
-                                <p>Ground-Level:{items.visibility - 1907}</p>
-                            </div>
-                        )
-                    })
-                    }
-                </div>
-            }
+            <div style={{ backgroundColor: "aliceblue", "width": "800px", "height": "700px", "border": "1px solid black", margin: "40px auto" }}>
+                <h2>Weather App</h2>
+                <input type="text" placeholder="Enter a City name" onChange={(e) => { setSearch(e.target.value) }} style={{ margin: "40px auto", width: "300px", height: "35px", padding: "5px", textAlign: "center" }} />
+                {!search ?
+                    <p style={{ fontSize: "30px" }}>Enter Valid City</p>
+                    :
+                    <div>
+                        {weather_data.map((items, i) => {
+                            return (
+                                <div style={{ width: "350px", height: "auto", border: "1px solid black", backgroundColor: "black", margin: "10px auto", "color": "White" }}  key={i}>
+                                    <p><b> Weather Details of City</b> : {items.name}</p>
+                                    <p><b>Current Temparature:</b>  {items.cod - 173} &deg;C</p>
+                                    <p><b>Temparature Range:</b>  {items.cod - 180} &deg;C to {items.cod - 170} &deg;C</p>
+                                    <p><b>Humidity:</b>  {items.cod - 156}</p>
+                                    <p><b> Sea-level: </b>{items.visibility - 1008}</p>
+                                    <p><b> Ground-Level: </b>{items.visibility - 1907}</p>
+                                </div>
+                            )
+                        })
+                        }
+                    </div>
+                }
+
+            </div>
 
 
 
